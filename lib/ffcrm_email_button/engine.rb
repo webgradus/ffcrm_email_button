740bca2b62c12ffde996a6b32010e69fb88c666a
GarPit <garik.piton@gmail.com>
2013-10-15 14:13:26 +0400

    fix module name in require

--- lib/ffcrm_email_button/engine.rb
+++ lib/ffcrm_email_button/engine.rb
@@ -1,7 +1,7 @@
 module FfcrmEmailButton
   class Engine < ::Rails::Engine
     config.to_prepare do
-      require 'ffcrm_sendgrid/email_button_view_hooks'
+      require 'ffcrm_email_button/email_button_view_hooks'
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
         Rails.application.config.cache_classes ? require(c) : load(c)
       end
